30c6cb14e04eec04d74fb93d37ec7fb28fc9e596
senbar <sendek.bartosz@gmail.com>
2021-11-14 16:56:15 +0100

    external dependency on react so not to load more than one react runtime

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = (webpackConfigEnv, argv) => {
      rules:[{
        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
      use:[{loader: 'url-loader?limit=100000'}]
-     }]}
+     }]},
+     externals: ['react', 'react-dom']
     // modify the webpack config however you'd like to by adding to this object
   });
 };
